8a96f532d7074345604bdc2956ad5034126dca7a
Lucho Morales <lmorales@dynamicdevs.io>
2024-02-07 18:52:29 -0500

    refact: type export

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  distDir: 'dist',
   images: { unoptimized: true },
   reactStrictMode: false,
   webpack(config) {
     const fileLoaderRule = config.module.rules.find((rule) =>
       rule.test?.test?.('.svg'),
-    )
+    );
 
-    config.module.rules.push(
-      {
-        test: /\.svg$/i,
-        issuer: fileLoaderRule.issuer,
-        resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] }, // exclude if *.svg?url
-        use: ['@svgr/webpack'],
-      },
-    )
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: fileLoaderRule.issuer,
+      resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] }, // exclude if *.svg?url
+      use: ['@svgr/webpack'],
+    });
     fileLoaderRule.exclude = /\.svg$/;
 
     return config;
